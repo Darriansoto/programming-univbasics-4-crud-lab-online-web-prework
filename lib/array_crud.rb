3e61bf4e4dd51c3828789330c0a0ec0cb9fd5f23
Darrian Soto <ddog350@gmail.com>
2019-09-04 05:40:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  
+  my_array = [red, blue, green, yellow]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  my_array = []
 end
 
 def add_element_to_start_of_array(array, element)
